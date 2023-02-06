b1e3e98deb1900f0c4314f44e8911b0169b990ab
Benjamin Spreng <benjamin.spreng@physik.uni-augsburg.de>
2023-02-05 18:44:56 -0800

    translation corrected, minor efficiency improvements

--- cascy_ded/cylinder_cylinder.py
+++ cascy_ded/cylinder_cylinder.py
@@ -41,17 +41,17 @@ class cylinder_cylinder_system:
         if mmax == None:
             mmax = int(eta_mmax * rho)
 
+        # precompute quadrature nodes and weights
         X1, W1 = self.z_quad(Nz)
         Kz = X1 / self.d
         Wz = W1 / self.d
 
+        X2, W2 = self.x_quad(Nx)
+        Kx = X2 / self.d
+        Wx = W2 / self.d
+
         result = 0.
         for i, kz in enumerate(Kz):
-            # precompute quadrature nodes and weights
-            X2, W2 = self.x_quad(Nx)
-            Kx = X2 / self.d
-            Wx = W2 / self.d
-
             # cylider reflection
             pwrc1 = pwrc_TMTM(mmax, kz * self.R1)
             R_cy1 = reflection_matrix(self.R1, kz, Kx, Wx, mmax, pwrc1)
@@ -61,11 +61,16 @@ class cylinder_cylinder_system:
                 pwrc2 = pwrc_TMTM(mmax, kz * self.R2)
                 R_cy2 = reflection_matrix(self.R2, kz, Kx, Wx, mmax, pwrc2)
 
+            # translation
             kappa = np.sqrt(Kx ** 2 + kz ** 2)
-            half_translation = np.diag(np.exp(-0.5*kappa * self.L))
+            half_translation = np.diag(np.exp(-0.5*kappa * self.d))
+
+            # round-trip
             M1 = half_translation @ R_cy1 @ half_translation
             M2 = half_translation @ R_cy2 @ half_translation
             M = M1 @ M2
+
+            # energy contribution
             result += Wz[i] / 2 / np.pi * logdet1m(M)
         return self.d/self.L*result
 
--- cascy_ded/plane_cylinder.py
+++ cascy_ded/plane_cylinder.py
@@ -38,24 +38,29 @@ class plane_cylinder_system:
         if mmax == None:
             mmax = int(eta_mmax * rho)
 
+        # precompute quadrature nodes and weights
         X1, W1 = self.z_quad(Nz)
         Kz = X1 / self.d
         Wz = W1 / self.d
 
+        X2, W2 = self.x_quad(Nx)
+        Kx = X2 / self.d
+        Wx = W2 / self.d
+
         result = 0.
         for i, kz in enumerate(Kz):
-            # precompute quadrature nodes and weights
-            X2, W2 = self.x_quad(Nx)
-            Kx = X2 / self.d
-            Wx = W2 / self.d
-
             # cylider reflection
             pwrc = pwrc_TMTM(mmax, kz * self.R)
             R_cy = reflection_matrix(self.R, kz, Kx, Wx, mmax, pwrc)
 
+            # translation
             kappa = np.sqrt(Kx ** 2 + kz ** 2)
-            translation = np.exp(-2 * kappa * self.L)
-            M = R_cy @ np.diag(-1. * translation)
+            translation = np.exp(-kappa * self.d)
+
+            # round-trip
+            M = np.diag(translation) @ R_cy @ np.diag(-1. * translation)
+
+            # energy contribution
             result += Wz[i] / 2 / np.pi * logdet1m(M)
         return self.d/self.L*result
 
