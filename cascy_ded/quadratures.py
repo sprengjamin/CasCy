92a1df410325797eed078b8b0efab92b873527fc
Benjamin Spreng <benjamin.spreng@physik.uni-augsburg.de>
2023-02-06 01:07:13 -0800

    fix issue of missing function

--- cascy_ded/quadratures.py
+++ cascy_ded/quadratures.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+def weights(N):
+    r"""Fourier-Chebyshev quadrature weights.
+
+    """
+    i = np.arange(1, N + 1, 1)
+    t = np.pi / (N + 1) * i
+    wts = np.zeros(N)
+    for j in i:
+        wts += np.sin(j * t) * (1 - np.cos(j * np.pi)) / j
+    wts *= 2 * np.sin(t) * (2 / (N + 1)) / (1 - np.cos(t)) ** 2
+    return wts
+
+
 def fcqs_semiinfinite(N):
     r"""Fourier-Chebyshev quadrature rule.
 
@@ -21,6 +34,7 @@ def fcqs_semiinfinite(N):
     pts = 1. / (np.tan(t / 2)) ** 2
     wts = weights(N)
     return pts, wts
+
 
 def fcqs_combined(N):
     k, w = fcqs_semiinfinite(N)
